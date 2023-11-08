a06bbec1a0687a10119af9c4f834e8d62dd4ac95
Alex <alexnyebuchan@gmail.com>
2023-11-08 14:42:31 +0000

    setup chat gpt and env

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from os import environ
 
 from flask import Flask, render_template, request, redirect
+
+from openai import OpenAI
 
 app = Flask(__name__)
 
@@ -9,7 +12,19 @@ def home():
         return render_template('index.html')
     elif request.method == 'POST':
         ingredients = request.form.get("ingredients")
-        print(ingredients)
+
+        req = "Here is what's in my kitchen: " + ingredients + ". What dishes could I make? Please provide a short list."
+
+        client = OpenAI(api_key=environ.get('API_KEY'))
+
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": req}
+        ]
+        )
+        print(completion.choices[0].message)
+
         return redirect('/')
 
 
