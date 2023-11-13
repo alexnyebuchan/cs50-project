68db710f4bf60b937afd49cb3271353173804c2e
Alex <alexnyebuchan@gmail.com>
2023-11-13 22:44:32 +0000

    added helpers, database

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from os import environ
+import sqlite3
 
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_session import Session
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 from openai import OpenAI
 
@@ -12,7 +16,10 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
+    user_id = session["user_id"]
+
     if request.method == "GET":
         return render_template('index.html')
     elif request.method == 'POST':
@@ -40,20 +47,93 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     session.clear()
-
     if request.method == "GET":
         return render_template('login.html')
     elif request.method == 'POST':
-        return
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if not username:
+            print('no username provided')
+            return redirect('/')
+        elif not password:
+            print('no password provided')
+            return redirect('/')
+
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+
+        row = cursor.execute("SELECT * FROM users WHERE name = ?", (username,)).fetchone()
+
+        if not row:
+            print('user does not exist')
+            return redirect('/')
+
+        if row[1] != username or not check_password_hash(row[2], (password)):
+            print('Invalid username or password')
+            return redirect('/')
+        
+        session["user_id"] = row[0]
+            
+
+        cursor.close()
+        connection.close()
+
+        print('Successfully logged in')
+
+        return redirect ('/')
     
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     session.clear()
 
     if request.method == "GET":
         return render_template('register.html')
     elif request.method == 'POST':
-        return
-    
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+
+        if not username:
+            print('no username provided')
+            return redirect('/')
+        elif not password:
+            print('no password provided')
+            return redirect('/')
+        elif not confirmation:
+            print('no confirmation provided')
+            return redirect('/')
+        
+        if password != confirmation:
+            print('password mismatch')
+            return redirect('/')
+
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+
+        cursor.execute('SELECT * FROM users')
+
+        pre_existing = cursor.execute("SELECT name FROM users WHERE name = ?", (username,)).fetchone()
+
+        if pre_existing:
+            print('user already exists')
+            return redirect('/')
+        hash = generate_password_hash(password)
+
+        cursor.execute("INSERT INTO users (name, hash, larder) VALUES (?, ?, ?)",
+            (username, hash, 'tobasco')
+            )
+        connection.commit()
+
+        rows = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        for row in rows:
+            print(row)
+
+        return redirect('/login')
+
 if __name__ == "__main__":
     app.run(debug=True)
